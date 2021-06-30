ebb60042db46241b3881940392e0f38e459378e1
RyoTani0210 <ryo.taniguchi@gmail.com>
2021-06-30 09:56:46 +0900

    fix colname and values to user input string

--- app.py
+++ app.py
@@ -71,38 +71,42 @@ def oa_design():
             # STEP3: Rから計画を出力する
             oa_design = pd.DataFrame(r.get("table"))
 
-            #この辺で置換処理を。。。
-
-            return oa_design
-
-        def makeViewFile(oa_design, file_name):
-            """
-            データフレーム(oa_design)をテーブルとして持ったhtmlファイルを作成する
-            """
-            # お試し用でコピペした https://qiita.com/nshinya/items/a46ef0002284d2f77789
-            html_template = """
-                <!doctype html>
-                <html lang="ja">
-                <head>
-                    <meta charset="utf-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-                </head>
-                <body>
-                    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-                    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-                    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-                    <div class="container">
-                        {table}
-                    </div>
-                </body>
-                </html>
+            #値の置換処理
+            ## 後からリファクタしやすいように関数化する
+            def makeValueMap(levels):
                 """
-            table = oa_design.to_html(classes=["table", "table-bordered", "table-hover"])
-            html = html_template.format(table=table)
+                arrayの値と元の入力値との対応を返す
+                入力 各水準の入力値が入った2重のリスト
+                出力 arrayの値がキー、元の入力値が値の辞書が入ったリスト
+                """
+                replace_levels = []
+                
+                for level in levels:
+                    # arrayの中と同じオブジェクトを生成する
+                    rep_key = [str_v.encode() for str_v in [str(v) for v in range(1,len(level)+1)]] #1始まり
+                    #対応関係の辞書
+                    value_map = dict(zip(rep_key, level))
+                    replace_levels.append(value_map)
+                    
+                return replace_levels
 
-            with open(file_name, "w") as f:
-                f.write(html)
+
+            #出力用df
+            new_oa_design = pd.DataFrame({"Exp No.": range(1,len(oa_design)+1)})
+
+            # 置換処理
+            ## 列名の置換
+            rep_col = dict(zip(oa_design.columns, labels))
+            oa_design = oa_design.rename(columns=rep_col)
+            
+            ## 値の置換
+            replace_levels = makeValueMap(levels)
+            
+            for col, replace_vals in zip(oa_design.columns, replace_levels):
+                sr_repl = oa_design[col].replace(replace_vals)
+                new_oa_design = pd.concat([new_oa_design,sr_repl], axis=1)
+                
+            return new_oa_design
 
         def makeResponse(oa_design):
             """
@@ -127,7 +131,7 @@ def oa_design():
                 </body>
                 </html>
                 """
-            table = oa_design.to_html(classes=["table", "table-bordered", "table-hover"])
+            table = oa_design.to_html(classes=["table", "table-bordered", "table-hover"], index=False)
             page_html = html_template.format(table=table)
 
             return page_html
